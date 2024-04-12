ff2d671e387bf452448dd19323956fb04c1bd356
Jessica Gates <gatespj@outlook.com>
2024-04-11 21:47:56 -0400

    lab for coffee registration

---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopRegistration.Models;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+
+        static List<User> users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                FirstName = "Jessica",
+                LastName = "Gates",
+                Email = "jesstest@email.com",
+                Password = "test1234"
+            },
+            new User
+            {
+                Id = 2,
+                FirstName = "Patrick",
+                LastName = "Gates",
+                Email = "pattest@email.com",
+                Password = "test1234"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = users.Max(x => x.Id) + 1;
+                users.Add(model);
+                return RedirectToAction("Summary", model);
+            }
+            return View(model); 
+        }
+
+        public IActionResult Summary(User model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50)]
+        [MinLength(8,ErrorMessage ="Password must be at least 8 characters.")]
+        public string Password { get; set; }
+
+    }
+}
